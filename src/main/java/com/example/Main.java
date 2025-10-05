ca22f2a2f4562d868381dfe901cacba08112dade
Edvin Sandgren <229709012+EdvinSandgren@users.noreply.github.com>
2025-10-05 14:38:03 +0200

    Day 3. Alternative (cleaner?) code. Passes all tests locally.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -37,57 +37,68 @@ public class Main {
     }
 
     private static void determineUsage(String[] args, ElpriserAPI priceList) {
-        ElpriserAPI.Prisklass zone;
-        switch (args[1]){
-            case "SE1" -> zone = ElpriserAPI.Prisklass.SE1;
-            case "SE2" -> zone = ElpriserAPI.Prisklass.SE2;
-            case "SE3" -> zone = ElpriserAPI.Prisklass.SE3;
-            case "SE4" -> zone = ElpriserAPI.Prisklass.SE4;
-            default -> {
-                printHelp();
-                System.out.println("Invalid zone input");
-                return;
-            }
-        }
-
-        if(args.length < 3){
-            printPricesForList(getMergedList(priceList, zone, LocalDate.now()));
-        } else if(args[2].equals("--charging")){
-            printChargePrice(getMergedList(priceList, zone, LocalDate.now()), args[3]);
-        } else if(args[2].equals("--sorted")){
-            printSortedPricesForList(getMergedList(priceList, zone, LocalDate.now()));
-        } else if(args[2].equals("--date")){
-            LocalDate parsedDate;
-            try{
-                parsedDate = LocalDate.parse(args[3]);
-            } catch (DateTimeParseException | IndexOutOfBoundsException e) {
-                printHelp();
-                System.out.println("Invalid date");
-                return;
-            }
-            if(args.length < 5){
-                printPricesForList(getMergedList(priceList, zone, parsedDate));
-            } else if(args[4].equals("--sorted")){
-                printSortedPricesForList(getMergedList(priceList, zone, parsedDate));
-            } else if(args[4].equals("--charging")){
-                printChargePrice(getMergedList(priceList, zone, parsedDate), args[5]);
-            } else{
-                printHelp();
-            }
+        if (args.length == 2) {
+            ElpriserAPI.Prisklass zone = getZone(args);
+            if (zone == null) return;
+            printPrices(getMergedList(priceList, zone, LocalDate.now()));
         } else {
-            printHelp();
+            switch (args.length < 5 ? args[2] : args[4]) {
+                case "--charging" -> printChargePrice(args, priceList);
+                case "--sorted" -> printSortedPrices(args, priceList);
+                case "--date" -> printPricesForDate(args, priceList);
+                default -> printHelp();
+            }
         }
     }
 
-    private static List<hourOfQuarters> getMergedList(ElpriserAPI priceList, ElpriserAPI.Prisklass zone, LocalDate date) {
-        List<ElpriserAPI.Elpris> mergedList = priceList.getPriser(date, zone);
-        mergedList.addAll(priceList.getPriser(date.plusDays(1), zone));
-        return convertQuartersToHours(mergedList);
+    private static void printPricesForDate(String[] args, ElpriserAPI priceList) {
+        LocalDate parsedDate = getParsedDate(args);
+        ElpriserAPI.Prisklass zone = getZone(args);
+        if (parsedDate == null || zone == null) return;
+
+        printPrices(getMergedList(priceList, zone, parsedDate));
     }
 
-    private static void printChargePrice(List<hourOfQuarters> prices, String args) {
+    private static void printSortedPrices(String[] args, ElpriserAPI priceList) {
+        LocalDate parsedDate = getParsedDate(args);
+        ElpriserAPI.Prisklass zone = getZone(args);
+        if (parsedDate == null || zone == null) return;
+
+        List<hourOfQuarters> sortedList = getMergedList(priceList, zone, parsedDate);
+        sortedList = sortedList.stream()
+                .sorted(Comparator.comparing(hourOfQuarters::price).reversed()
+                        .thenComparing(hourOfQuarters::startDate))
+                .toList();
+
+        printPrices(sortedList);
+    }
+
+    private static void printPrices(List<hourOfQuarters> priceList) {
+        if(priceList.isEmpty()){
+            System.out.println("Found no data");
+        } else {
+            double lowestPrice = Double.MAX_VALUE, highestPrice = 0, meanPrice = 0;
+            System.out.println("Prislista:");
+            for (hourOfQuarters price : priceList) {
+                lowestPrice = Math.min(lowestPrice, price.price());
+                highestPrice = Math.max(highestPrice, price.price());
+                meanPrice += price.price();
+                System.out.printf(formattedTime(price) + " %.2f öre\n", price.price() * 100);
+            }
+            System.out.printf("Lägsta pris: %.02f öre\n", lowestPrice*100);
+            System.out.printf("Högsta pris: %.02f öre\n", highestPrice*100);
+            System.out.printf("Medelpris: %.02f öre\n", meanPrice/priceList.size()*100);
+        }
+    }
+
+    private static void printChargePrice(String[] args, ElpriserAPI priceList) {
+        ElpriserAPI.Prisklass zone = getZone(args);
+        if(zone == null){
+            return;
+        }
+
         //sliding window algorithm, 2h 4h 8h
-        int chargeDuration = switch(args){
+        int chargeDuration = switch(args.length < 5 ? args[3] : args[5]){
             case "2h" -> 2;
             case "4h" -> 4;
             case "8h" -> 8;
@@ -99,7 +110,12 @@ public class Main {
             return;
         }
 
-        List<hourOfQuarters> chargeWindow = prices.subList(0,chargeDuration);
+        LocalDate parsedDate = getParsedDate(args);
+        if (parsedDate == null) return;
+
+        List<hourOfQuarters> prices = getMergedList(priceList, zone, parsedDate);
+        List<hourOfQuarters> chargeWindow;
+        int indexWindow = chargeDuration;
         double lowestSum = 0;
 
         for (int i = 0; i < chargeDuration; i++) {
@@ -110,48 +126,15 @@ public class Main {
         for (int i = chargeDuration; i < prices.size(); i++) {
             windowSum += prices.get(i).price() - prices.get(i - chargeDuration).price();
             if(windowSum < lowestSum){
-                chargeWindow = prices.subList(i+1 - chargeWindow.size(), i+1);
+                indexWindow = i+1;
                 lowestSum = windowSum;
             }
         }
 
+        chargeWindow = prices.subList(indexWindow - chargeDuration, indexWindow);
+
         System.out.printf("Medelpris för fönster: %.2f öre\n", lowestSum/chargeDuration*100);
         System.out.printf("Påbörja laddning kl %02d:00\n", chargeWindow.getFirst().startDate.getHour());
-    }
-
-    private static void printSortedPricesForList(List<hourOfQuarters> sortedList) {
-        sortedList = sortedList.stream()
-                .sorted(Comparator.comparing(hourOfQuarters::price).reversed()
-                        .thenComparing(hourOfQuarters::startDate))
-                .toList();
-
-        printPricesForList(sortedList);
-    }
-
-    private static void printPricesForList(List<hourOfQuarters> pricelist) {
-        if(pricelist.isEmpty()){
-            System.out.println("Found no data");
-        } else {
-            double lowestPrice = Double.MAX_VALUE, highestPrice = 0, meanPrice = 0;
-            System.out.println("Prislista:");
-            for (hourOfQuarters price : pricelist) {
-                lowestPrice = Math.min(lowestPrice, price.price());
-                highestPrice = Math.max(highestPrice, price.price());
-                meanPrice += price.price();
-                System.out.printf(formattedTime(price) + " %.2f öre\n", price.price() * 100);
-                //System.out.printf("Mellan " + formattedStartTime(price) + " och " + formattedEndTime(price) + ": %.1f öre/KWh\n", price.sekPerKWh()*100);
-            }
-            System.out.printf("Lägsta pris: %.02f öre\n", lowestPrice*100);
-            System.out.printf("Högsta pris: %.02f öre\n", highestPrice*100);
-            System.out.printf("Medelpris: %.02f öre\n", meanPrice/pricelist.size()*100);
-        }
-    }
-
-    private static String formattedTime(hourOfQuarters source){
-        String formattedTime;
-        formattedTime = String.format("%02d-%02d", source.startDate.getHour(), source.endDate.getHour());
-        //startTime = String.format("%02d", source.timeStart().getHour()) + ":" + String.format("%02d", source.timeStart().getMinute());
-        return formattedTime;
     }
 
     private static List<hourOfQuarters> convertQuartersToHours(List<ElpriserAPI.Elpris> prices) {
@@ -178,6 +161,52 @@ public class Main {
         return hourList;
     }
 
+    private static ElpriserAPI.Prisklass getZone(String[] args) {
+        ElpriserAPI.Prisklass zone = null;
+        try {
+            switch (args[1]){
+                case "SE1" -> zone = ElpriserAPI.Prisklass.SE1;
+                case "SE2" -> zone = ElpriserAPI.Prisklass.SE2;
+                case "SE3" -> zone = ElpriserAPI.Prisklass.SE3;
+                case "SE4" -> zone = ElpriserAPI.Prisklass.SE4;
+                default -> {
+                    printHelp();
+                    System.out.println("Invalid zone input");
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            printHelp();
+        }
+        return zone;
+    }
+
+    private static LocalDate getParsedDate(String[] args) {
+        LocalDate parsedDate;
+        if(args.length < 4){
+            parsedDate = LocalDate.now();
+        }else{
+            try {
+                parsedDate = LocalDate.parse(args[3]);
+            } catch (DateTimeParseException | IndexOutOfBoundsException e) {
+                printHelp();
+                System.out.println("Invalid date");
+                return null;
+            }
+        }
+        return parsedDate;
+    }
+
+    private static List<hourOfQuarters> getMergedList(ElpriserAPI priceList, ElpriserAPI.Prisklass zone, LocalDate date) {
+        List<ElpriserAPI.Elpris> mergedList = priceList.getPriser(date, zone);
+        mergedList.addAll(priceList.getPriser(date.plusDays(1), zone));
+        return convertQuartersToHours(mergedList);
+    }
+
+    private static String formattedTime(hourOfQuarters source){
+        String formattedTime;
+        formattedTime = String.format("%02d-%02d", source.startDate.getHour(), source.endDate.getHour());
+        return formattedTime;
+    }
+
     record hourOfQuarters(ZonedDateTime startDate, ZonedDateTime endDate, double price){}
-    //record hourOfQuarters(int startHour, int endHour, double price){}
 }
